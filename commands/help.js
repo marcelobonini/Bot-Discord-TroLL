a53ee7eb8f1f2cfc6548d15d9cb75ee8d51c98e7
Marcelo Cruz <marcelobonini99@hotmail.com>
2021-07-15 12:11:16 -0300

    primeirocommit

---
+++ commands/help.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args) => {
+    const embed = new Discord.MessageEmbed()
+    .setColor('#d48d13')
+    .setDescription(` Minha lista de comandos ${message.author}`)
+    .setTimestamp()
+    .setFooter(` Autor: ${message.author.username} `)
+    .addFields(
+        {
+            name: '.avatar',
+            value: `Exibir o avatar da pessoa desejada`,
+            
+        },
+        {
+            name: '.clear',
+            value: `Limpa o chat de 0 a 99 mensagens`,
+            
+        },
+         {
+            name: '.kick',
+            value: `Kickar o amiguinho`,
+            
+        },
+        {
+            name: '.ban',
+            value: `Banir o amiguinho`,
+            
+        },
+    )
+    message.channel.send(embed);
+}
